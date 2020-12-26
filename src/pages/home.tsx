0332668a4004d9647fbc61183e977922f7a43852
Javier Segovia <javiersegoviaa29@gmail.com>
2020-12-26 07:37:21 -0400

    Add eslint-plugin-react-hooks

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,17 +2,19 @@ import Link from 'next/link'
 import styled from 'styled-components'
 
 const Title = styled.h1`
-  color: blue;
+  font-weight: bold;
+  color: black;
 `
 
 const Button = styled.button`
   color: black;
+  margin-top: 1.875rem;
 `
 
 export default function Home() {
   return (
     <>
-      <Title className="text-center text-4xl">Slatam Application</Title>
+      <Title className="text-center text-4xl">NextJS Starter Project</Title>
       <Link href="/products">
         <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Product page
--- tests/pages/home/index.test.tsx
+++ tests/pages/home/index.test.tsx
@@ -5,25 +5,14 @@ import Home from '@pages/index'
 describe('Home page', () => {
   it('matches snapshot', () => {
     const { asFragment } = render(<Home />)
-    expect(asFragment()).toMatchInlineSnapshot(`
-      <DocumentFragment>
-        <h1
-          class="pages__Title-sc-18u934-0 iKuUDQ text-center text-4xl"
-        >
-          Slatam Application
-        </h1>
-        <button
-          class="pages__Button-sc-18u934-1 ehqjVe bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Product page
-        </button>
-      </DocumentFragment>
-    `)
+    expect(asFragment()).toMatchInlineSnapshot()
   })
 
   it('should have the correct title heading', async () => {
     render(<Home />)
     // fireEvent.click(screen.getByText('Test Button'))
-    expect(await screen.findByText(/product page/i)).toBeInTheDocument()
+    expect(
+      await screen.findByText(/nextJS starter project/i)
+    ).toBeInTheDocument()
   })
 })
